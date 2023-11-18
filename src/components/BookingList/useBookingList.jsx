1b0dfd66ca1a956662e6e028754a1ebd75cb997f
fabio <fabio@digitalmind.com>
2023-11-18 20:41:26 -0300

    [ADD] Retrieving Initial Data from MockAPI

--- src/components/BookingList/useBookingList.jsx
+++ src/components/BookingList/useBookingList.jsx
@@ -3,14 +3,41 @@ import { useState, useEffect } from 'react'
 export default function useBookingList() {
   const [bookings, setBookings] = useState([])
   const [selectedBooking, setSelectedBooking] = useState(null)
+  const url = 'https://654a58cae182221f8d5304a9.mockapi.io/data/Tickets'
 
-  const fetchBookings = () => {
+  const fetchBookings = async () => {
+    // Try to get data from local storage
     const storedBookings = JSON.parse(localStorage.getItem('bookingData')) || []
-    setBookings(storedBookings)
+
+    // If there is no data in local storage, fetch data from the URL
+    if (storedBookings.length === 0) {
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error('Failed to fetch data')
+        }
+
+        const data = await response.json()
+        console.log(data);
+        
+        // Update local storage with fetched data
+        localStorage.setItem('bookingData', JSON.stringify(data))
+
+        // Update state with fetched data
+        setBookings(data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    } else {
+      // If there is data in local storage, update state with that data
+      setBookings(storedBookings)
+    }
   }
 
   useEffect(() => {
-    fetchBookings()
+    setTimeout(() => {
+      fetchBookings()
+    }, 1000)
   }, [])
 
   const handleItemClick = (index) => {
